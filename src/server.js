e5cd871951a64781d986a0c4795cf8cd9a04f777
asiar123 <michael_2504@hotmail.com>
2024-05-13 16:59:00 -0500

    5 commit

--- src/server.js
+++ src/server.js
@@ -17,10 +17,8 @@ const httpsServer = https.createServer(credentials, app);
 // Configuración de Socket.io
 const io = require('socket.io')(httpsServer, {
   cors: {
-    origin: "https://frontened-s7n0.onrender.com", // Cambia aquí el origen
+    origin: 'https://frontened-s7n0.onrender.com', // Cambia aquí el origen
     methods: ["GET", "POST"],
-    allowedHeaders: ["my-custom-header"],
-    credentials: true
   }
 });
 
