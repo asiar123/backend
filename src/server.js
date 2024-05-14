f395042fd06bb79a2897b04965cad0b90e6d8557
asiar123 <michael_2504@hotmail.com>
2024-05-13 19:43:24 -0500

    8 commit

--- src/server.js
+++ src/server.js
@@ -1,7 +1,79 @@
-const { app, httpsServer, io } = require('./server');
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const pool = require('./config/dbConfig');
+const https = require('https');
+const fs = require('fs');
+const socketIo = require('socket.io');
 
-const port = process.env.PORT || 7000;
+const app = express();
 
-httpsServer.listen(port, '0.0.0.0', () => {
-  console.log(`Servidor HTTPS ejecutándose en https://192.168.1.13:${port}`);
+// Configuración de HTTPS
+const privateKey = fs.readFileSync('localhost.key', 'utf8');
+const certificate = fs.readFileSync('localhost.crt', 'utf8');
+const credentials = { key: privateKey, cert: certificate };
+const httpsServer = https.createServer(credentials, app);
+
+// Configuración de Socket.io
+const io = require('socket.io')(httpsServer, {
+  cors: {
+    origin: "https://frontened-s7n0.onrender.com",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true
+  }
 });
+
+io.on('connection', (socket) => {
+  console.log(`Usuario conectado: ${socket.id}`);
+
+  // Evento personalizado que el cliente debería emitir después de iniciar sesión
+  socket.on('registerUser', async (id_usuario) => {
+    try {
+      await pool.query(
+        'UPDATE usuarios SET socket_id = $1 WHERE id_usuario = $2',
+        [socket.id, id_usuario]
+      );
+      console.log(`Usuario ${id_usuario} registrado con socket ID ${socket.id}`);
+    } catch (err) {
+      console.error('Error al registrar socket ID:', err);
+    }
+  });
+
+  socket.on('disconnect', async () => {
+    try {
+      await pool.query(
+        'UPDATE usuarios SET socket_id = NULL WHERE socket_id = $1',
+        [socket.id]
+      );
+      console.log(`Socket ID ${socket.id} ha sido removido`);
+    } catch (err) {
+      console.error('Error al remover socket ID:', err);
+    }
+  });
+});
+
+app.use(express.json());
+app.use(cors());
+
+const geolocationRoutes = require('./routes/geolocationRoutes')(io);
+const authRoutes = require('./routes/authRoutes');
+
+app.use('/api/auth', authRoutes);
+app.use('/api/geolocation', geolocationRoutes);
+
+app.get('/', (req, res) => {
+  res.send('¡Hola Mundo!');
+});
+
+app.get('/db', async (req, res) => {
+  try {
+    const response = await pool.query('SELECT NOW()');
+    res.json(response.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error al conectar con la base de datos');
+  }
+});
+
+module.exports = { app, httpsServer, io };
