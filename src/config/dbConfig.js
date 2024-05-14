fa6734787522c13b8137871f8f16728f6fb2e93a
asiar123 <michael_2504@hotmail.com>
2024-05-13 21:00:05 -0500

    9 commit

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
@@ -7,6 +6,9 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASS,
   port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false // Esta opción permite conexiones a servidores que no tienen certificados verificados. Usar con precaución.
+  }
 });
 
 module.exports = pool;
